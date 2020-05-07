f1bbeceebc691bd2d2f4d202e4a035a4027c2d77
Joann Pan <joannlpan@gmail.com>
2020-05-07 15:52:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,4 +13,24 @@ class School
      roster[grade] << "#{name}"
   end
 
+  def grade(num)
+    roster(num)
+  end
+# => {9 => ["AC Slater", "Zach Morris"], 
+#10 => ["Aardvark", "Kelly Kapowski"], 
+#11 => ["Screech", "Xavier"]}
+
+
+  def sort
+    sorted = {}
+    roster.each do |grade, names|
+      sorted[grade] = names.sort
+    end
+    sorted
+  end
+      
+
+  end
+    
+
 end
