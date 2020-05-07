36d5dfeb43d98291ebac976df61cc72f476b1b75
Joann Pan <joannlpan@gmail.com>
2020-05-07 15:18:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+require "pry"
+class School
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    roster = {} #will contain keys of grade levels, value will be array of student names
+
+  end
+
+  def add_student(name, grade)
+     hash["grade"] = []
+     hash["grade"] << "#{name}"
+     hash
+  end
+
+end
